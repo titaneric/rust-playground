18ec0a1359c31ea8ff9a3b35e2efdad5546e2c08
Jake Goulding <jake.goulding@integer32.com>
2019-08-18 11:21:32 -0400

    Playground

--- ui/frontend/Playground.tsx
+++ ui/frontend/Playground.tsx
@@ -7,27 +7,31 @@ import Notifications from './Notifications';
 import Output from './Output';
 import * as selectors from './selectors';
 import State from './state';
+import { Orientation } from './types';
+
+import styles from './Playground.module.css';
+const orientationMap = {
+  [Orientation.Automatic]: styles.splitAutomatic,
+  [Orientation.Horizontal]: styles.splitHorizontal,
+  [Orientation.Vertical]: styles.splitVertical,
+};
 
 const Playground: React.SFC = () => {
   const showNotifications = useSelector(selectors.anyNotificationsToShowSelector);
   const focus = useSelector((state: State) => state.output.meta.focus);
   const splitOrientation = useSelector((state: State) => state.configuration.orientation);
 
-  const outputFocused = focus ? 'playground-output-focused' : '';
-  const splitClass = 'playground-split';
-  const orientation = splitClass + '-' + splitOrientation;
-
   return (
     <div>
-      <div className="playground">
-        <div className="playground-header">
+      <div className={styles.container}>
+        <div>
           <Header />
         </div>
-        <div className={`${splitClass} ${orientation}`}>
-          <div className="playground-editor">
+        <div className={orientationMap[splitOrientation]}>
+          <div className={styles.editor}>
             <Editor />
           </div>
-          <div className={`playground-output ${outputFocused}`}>
+          <div className={focus && styles.outputFocused}>
             <Output />
           </div>
         </div>
